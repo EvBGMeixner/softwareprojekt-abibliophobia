89ada5b69e9fa4f23edda4e07d2b5d78f1f013a9
Miriam <miriam.freimann@student.evbg.de>
2025-05-30 10:30:21 +0200

    hi

--- Buch.java
+++ Buch.java
@@ -5,7 +5,7 @@
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class Buch
+public class Buch extends RECHTECK
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private int x;
