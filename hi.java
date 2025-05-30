f954068a68b3c146f5d2d593551a1ceaad797be2
AnnaLena <Anna.Richter@student.evbg.de>
2025-05-30 09:31:58 +0200

    hi

---
+++ hi.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse hi.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class hi
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse hi
+     */
+    public hi()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
