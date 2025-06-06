d15c94e033e2b6dbd06c71fc3a16947c288219cc
AnnaLena <Anna.Richter@student.evbg.de>
2025-06-06 09:17:08 +0200

    hi

--- Bibliothekarin.java
+++ Bibliothekarin.java
@@ -9,6 +9,7 @@ public class Bibliothekarin extends FIGUR
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private int x;
+    double deltaX;
 
     /**
      * Konstruktor für Objekte der Klasse Bibliothekarin
@@ -17,8 +18,24 @@ public class Bibliothekarin extends FIGUR
     {
         super( zustandName , gifPfad );
         x = 0;
+        deltaX = 0;
     }
-
+    
+    void bewegen() {
+        verschiebenUm(deltaX, 0);
+    }
+    
+    void bewegeNachLinks() {
+        deltaX = -0.2;
+    }
+    
+    void bewegeNachRechts() {
+        deltaX = 0.2;
+    }
+    
+    void anhalten() {
+        deltaX = 0;
+    }
     /**
      * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
      * 
