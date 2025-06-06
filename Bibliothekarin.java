11de6188034575057e9db3e790996cc2d87e95d8
AnnaLena <Anna.Richter@student.evbg.de>
2025-06-06 09:30:40 +0200

    hi

--- Bibliothekarin.java
+++ Bibliothekarin.java
@@ -36,15 +36,5 @@ public class Bibliothekarin extends FIGUR
     void anhalten() {
         deltaX = 0;
     }
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+
 }
--- Buch.java
+++ Buch.java
@@ -1,33 +1,14 @@
-
-/**
- * Beschreiben Sie hier die Klasse Buch.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class Buch extends RECHTECK
+public class Buch extends RECHTECK 
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Buch
-     */
+    double breite;
+    double höhe;
+    
     public Buch()
     {
-        // Instanzvariable initialisieren
-        x = 0;
+       breite= 5;
+       höhe= 3;
+       setzeFarbe("braun");
+       
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
 }
