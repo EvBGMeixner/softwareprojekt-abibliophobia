d60900b574cfb9285fcdd8fd12190bef2a89f0f1
AnnaLena <Anna.Richter@student.evbg.de>
2025-07-04 09:08:10 +0200

    hi

--- ABIBLIO.java
+++ ABIBLIO.java
@@ -1,37 +1,22 @@
 
 public class ABIBLIO extends SPIEL
 {
-<<<<<<< HEAD
     Buch buch;
     SpeziellesBuch SpBuch;
     SuperBuch SuBuch;
     Bibliothekarin spieler;
-    
 
-=======
->>>>>>> 6385d7e71354fbee686aeab09aa67e27979ea7f2
-    
-<<<<<<< HEAD
     public ABIBLIO()
-    { new RECHTECK(6,4);
-=======
-   
-    Buch buch;
-    SpeziellesBuch spezi;
-    SuperBuch superb;
-    Bibliothekarin spieler;
-    BODEN boden;
+    {   new RECHTECK(6,4);
+        String zustandName ;
+        String gifPfad ;
     
-    String zustandName ;
-    String gifPfad ;
-    public ABIBLIO(){
          zeigeKoordinatensystem(true);
         //buch=new Buch( "bu" , gifPfad);
         //spezi=new SpeziellesBuch("spbu", gifPfad);
         //superb=new SuperBuch("subu", gifPfad);
         boden=new BODEN();
->>>>>>> 6385d7e71354fbee686aeab09aa67e27979ea7f2
-        
+    
     }
     
     public int beispielMethode()
